e704bf0fbc83e73925893e70f92e403c2fde2ad6
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-03-01 19:50:47 -0600

    Creación array frutas

--- frutas.js
+++ frutas.js
@@ -1,10 +1,20 @@
 /*Problema: Clasificación de Frutas
 Imagina que tienes un programa que clasifica las frutas según su tipo y cuenta cuántas hay de cada tipo.
-
 Instrucciones para resolver el problema:
-Declara un arreglo llamado frutas con varios tipos de frutas.
-Crea un objeto para almacenar la cantidad de cada tipo de fruta.
-Usa un ciclo for/while para recorrer el arreglo y contar las frutas.
-Imprime en la consola la cantidad de cada tipo de fruta.
+   + Declara un arreglo llamado frutas con varios tipos de frutas.
+   + Crea un objeto para almacenar la cantidad de cada tipo de fruta.
+   + Usa un ciclo for/while para recorrer el arreglo y contar las frutas.
+   + Imprime en la consola la cantidad de cada tipo de fruta.
 Opcional: intenta implementar la solución con el otro ciclo también (for/while).*/
 
+//array de frutas
+const frutas = ['manzana', 'fresa', 'sandia', 'piña', 'sandia', 'fresa', 'piña', 'manzana',
+    'piña', 'sandia', 'fresa', 'sandia', 'manzana', 'fresa', 'piña', 'fresa', 'sandia'];
+
+//array con objetos tipo de fruta
+const tipoF =[];
+
+//for par recorrer frutas
+for (let index = 0; index < frutas.length; index++) {
+    
+}
