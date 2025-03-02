2a82c8fa91bb5590c1ec16685c7e9b65be3aacfd
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-03-01 19:56:51 -0600

    Archivo js con los ejercicios de las lecturas

---
+++ EjerciciosPracticos.js
@@ -0,0 +1,10 @@
+/*Intenta practicar el uso de este ciclo con los siguientes ejercicios:
+    + Crea un ciclo while que cuente del 10 al 1 y lo imprima en la consola.
+    + Modifica el ciclo para que imprima solo los números pares.*/
+
+let i = 10;
+
+while (i > 0) {
+    console.log(i);
+    i --;
+}
