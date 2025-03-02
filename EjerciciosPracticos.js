7acb11ca238f68b45863d2d7f0a8b712229f0433
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-03-01 20:13:35 -0600

    Ejercicios con ciclo while

--- EjerciciosPracticos.js
+++ EjerciciosPracticos.js
@@ -1,10 +1,20 @@
-/*Intenta practicar el uso de este ciclo con los siguientes ejercicios:
+/*EJERCICIOS WHILE
+Intenta practicar el uso de este ciclo con los siguientes ejercicios:
     + Crea un ciclo while que cuente del 10 al 1 y lo imprima en la consola.
     + Modifica el ciclo para que imprima solo los números pares.*/
 
+//EJERCICIO 1
 let i = 10;
 
-while (i > 0) {
+/*while (i > 0) {
     console.log(i);
     i --;
-}
+} */
+
+//EJERCICIO 2
+while (i > 0) {
+    if (i%2 === 0) {
+        console.log(i);
+    }
+    i --;
+} 
